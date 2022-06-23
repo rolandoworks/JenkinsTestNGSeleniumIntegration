5f3571e9189b728660662fa20379e0b838d5f6cd
rolandoworks <rolandoworks@gmail.com>
2022-06-23 19:36:04 -0400

    init(project) TestNG and Selenium sample project

---
+++ src/com/rdiaz/training/testng/SampleSeleniumTest.java
@@ -0,0 +1,52 @@
+package com.rdiaz.training.testng;
+
+import java.util.Arrays;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SampleSeleniumTest {
+	
+	WebDriver driver;
+	
+	@BeforeClass
+	public void startBrowser() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver_linux64/chromedriver");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setBinary("/usr/bin/google-chrome-stable");
+		chromeOptions.addArguments("--headless");
+		//chromeOptions.addArguments("--start-maximized");
+	    chromeOptions.addArguments("--no-sandbox");
+	    chromeOptions.addArguments("--disable-gpu");
+		driver = new ChromeDriver(chromeOptions);
+	}
+	
+	@Test
+	public void validateGoogleId() throws Exception {
+		System.out.println("Opening Browser");
+		driver.get("https://www.google.com");
+		System.out.println("Clicking Gmail Link");
+		driver.findElement(By.xpath("//*[@id=\"gb\"]/div/div[1]/div/div[1]/a")).click();
+		System.out.println("Clicking Sign In link");
+		//driver.findElement(By.xpath("/html/body/header/div/div/div/a[2]")).click();
+		System.out.println("Entering username");
+		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("rolandoworks@gmail.com");
+		System.out.println("Clicking Next button");
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]")).click();
+		//driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
+		Thread.sleep(5000);
+		boolean textFound = driver.getPageSource().contains("This browser or app may not be secure");
+		AssertJUnit.assertTrue(textFound);
+	}
+	
+	@AfterClass
+	public void closeBrowser() {
+		driver.quit();
+	}
+}
