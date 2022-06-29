da132175aef4f70c876fb5bd3e5fb14a038f19ce
rolandoworks <rolandoworks@gmail.com>
2022-06-29 18:39:24 -0400

    clean(test) refactored code

--- src/com/rdiaz/training/testng/SampleSeleniumTest.java
+++ src/com/rdiaz/training/testng/SampleSeleniumTest.java
@@ -1,6 +1,8 @@
 package com.rdiaz.training.testng;
 
 
+import java.util.Arrays;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,10 +21,14 @@ public class SampleSeleniumTest {
 		System.setProperty("webdriver.chrome.driver", "bin/chromedriver_linux64/chromedriver");
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.setBinary("/usr/bin/google-chrome-stable");
-		chromeOptions.addArguments("--headless");
-		//chromeOptions.addArguments("--start-maximized");
-	    chromeOptions.addArguments("--no-sandbox");
-	    chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments(Arrays.asList(
+                "--headless",
+                "--disable-dev-shm-usage",
+                "--window-size=1920,1040",
+                "--no-sandbox",
+                "--disable-gpu",
+                "--allow-running-insecure-content"
+        ));
 		driver = new ChromeDriver(chromeOptions);
 	}
 	
@@ -30,18 +36,30 @@ public class SampleSeleniumTest {
 	public void validateGoogleId() throws Exception {
 		System.out.println("Opening Browser");
 		driver.get("https://www.google.com");
+		
+		bypassCaptcha();
 		System.out.println("Clicking Gmail Link");
 		driver.findElement(By.xpath("//*[@id=\"gb\"]/div/div[1]/div/div[1]/a")).click();
-		System.out.println("Clicking Sign In link");
-		//driver.findElement(By.xpath("/html/body/header/div/div/div/a[2]")).click();
+
+		bypassCaptcha();
 		System.out.println("Entering username");
 		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("rolandoworks@gmail.com");
+		
+		bypassCaptcha();
 		System.out.println("Clicking Next button");
 		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]")).click();
-		//driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
-		Thread.sleep(5000);
+		
+		bypassCaptcha();
 		boolean textFound = driver.getPageSource().contains("This browser or app may not be secure");
 		AssertJUnit.assertTrue(textFound);
+	}
+	
+	private void bypassCaptcha() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@AfterClass
